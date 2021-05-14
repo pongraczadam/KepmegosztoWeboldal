c0dc578b2abee71f6bfd5032d9f63a629e6b4322
Pongrácz Ádám <9999adam99@gmail.com>
2021-05-14 17:46:46 +0200

    Az oldal fő szerkezete. User oldalak. User adatbázis táblas.

---
+++ Weboldal/test/controllers/users_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "should get login" do
+    get users_login_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get users_new_url
+    assert_response :success
+  end
+
+  test "should get forgotten" do
+    get users_forgotten_url
+    assert_response :success
+  end
+end
