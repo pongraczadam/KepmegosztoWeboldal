35858e794123361f9e7d9c70727087572431da99
Pongrácz Ádám <9999adam99@gmail.com>
2021-05-22 21:32:24 +0200

    Unit tesztek

--- Weboldal/test/models/user_test.rb
+++ Weboldal/test/models/user_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "cannot_save_user_without_name" do
+    u = User.new email: 'valaki@mail.bme.hu'
+    assert !u.save, "Probléma adódott a teszt futtatása során" 
+  end
+
+  test "cannot_save_user_without_email" do
+    u = User.new name: 'Valaki'
+    assert !u.save, "Probléma adódott a teszt futtatása során" 
+  end
+
+  test "cannot_save_user_without_existing_email" do
+    u = User.new email: users(:one).email
+    assert !u.save, "Probléma adódott a teszt futtatása során" 
+  end
+
+  test "encrypted" do
+    u = User.find users(:one).id
+    assert_equal u.encrypted_password, Digest::SHA1.hexdigest('titoktitok'), "A titkosítás nem megfelelő"
+  end
 end
