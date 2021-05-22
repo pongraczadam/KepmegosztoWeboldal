508b880b13bfc9f9837503f2c07afd704f0437ec
Pongrácz Ádám <9999adam99@gmail.com>
2021-05-22 22:29:58 +0200

    Elírás javítva

--- Weboldal/test/models/tag_test.rb
+++ Weboldal/test/models/tag_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class TagTest < ActiveSupport::TestCase
-    test "cannot_save_favourite_with_existing" do
+    test "cannot_save_tag_with_existing" do
         t = Tag.new(name: tags(:one).name, image_id: tags(:one).image_id)
         assert !t.save, "A tag elmentődött" 
     end
